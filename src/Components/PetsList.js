df19c300e3c4844d1b36f89325bed01aa0226189
Abdullah Abbas <sl_amg@msn.com>
2024-11-12 19:38:47 +0300

    Task: React Filtering Pets completed

--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import pets from "../petsData";
 import PetItem from "./PetItem";
+import petsData from "../petsData";
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
 
+  const [query, setQuery]= useState (""); //create a state called query and a method called setQuery. The initial value should be “” this should be at top
+
+  const [type, setType]= useState (""); // always on top
+
+  let filterDataPets=pets.filter((pet)=>pet.name.includes(query)); // to filter, we make a new const called filteredDatapet and givt it a 
+  // filter on pets from petsData .. and to filetr it it should check the name at pet.name includes the query that was inputted
+  // filter before map 
+
+  //now filter again by type
+  let filterPetType =filterDataPets.filter((pet)=>pet.type.includes(type));
+
+  const petList = filterPetType.map((pet) => <PetItem pet={pet} key={pet.id} />);
+// to filter, we use the anarky .. thereofr first we filter by name and then by type
+  
+  console.log(query)
   return (
     <section id="doctors" className="doctor-section pt-140">
       <div className="container">
@@ -20,11 +36,12 @@ function PetsList() {
                   placeholder="Search"
                   aria-label="Search"
                   aria-describedby="search-addon"
+                  onChange={(event)=> setQuery(event.target.value)} // created an onChange with input event that calles setQuery and gives it event.tagrt.value
                 />
               </div>
               <br />
               Type:
-              <select className="form-select">
+              <select className="form-select" onChange={(event)=>setType(event.target.value)}> {/* to add a filter by type */}
                 <option value="" selected>
                   All
                 </option>
